a716456a4c983364434cbc5bffc2b2513e87ffda
Petar Mlinarić <petar.mlinaric@math.hr>
2026-06-01 15:40:14 +0200

    Remove polyfill

--- conf.py
+++ conf.py
@@ -1260,7 +1260,6 @@ SHOW_SOURCELINK = False
 # before </head>
 # (translatable)
 EXTRA_HEAD_DATA = r"""
-<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
 <script id="MathJax-script" async
 src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 """
